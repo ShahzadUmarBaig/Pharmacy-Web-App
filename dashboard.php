ccb56251560d133d345f0ac0e0e5ab86c2e8a9bc
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2020-08-28 11:57:41 +0500

    added a converter file

--- dashboard.php
+++ dashboard.php
@@ -81,6 +81,12 @@
           <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Customers
+                </a>
+              </li>
+              <li class="nav-item">
                 <a class="nav-link" href="#orders">
                   <span data-feather="file"></span>
                   Orders
@@ -90,12 +96,6 @@
                 <a class="nav-link" href="#">
                   <span data-feather="shopping-cart"></span>
                   Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users"></span>
-                  Customers
                 </a>
               </li>
             </ul>
@@ -109,22 +109,71 @@
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>ID</th>
+                  <th>Email</th>
+                  <th>Password</th>
+                  <th>Contact</th>
+                  <th>isAdmin</th>
+                  <th class="text-center">Delete</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
+                  <?php
+                      $servername = "localhost";
+                      $username = "root";
+                      $serverpass = "";
+                      $dbname = "MedKube";
+
+                      // Create connection
+                      $conn = new mysqli($servername, $username, $serverpass, $dbname);
+                      // Check connection
+                      
+                      if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                      
+                      $sql = "SELECT * FROM users";
+                      $result = $conn->query($sql);
+                      
+                      if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                          echo $row["ID"]. $row["Email"]. $row["Password"]. $row["Contact"]. "<br>";
+                          echo "<td class=".$row["ID"].">".trim($row["ID"]," ")."</td>
+                          <td>".$row["Email"]."</td>
+                          <td>".$row["Password"]."</td>
+                          <td>".$row["Contact"]."</td>";
+                          if($row["isAdmin"] == 1){
+                            echo "<td>true</td>";}else {
+                              echo "<td>false</td>";
+                            }
+                          echo "<td class='text-center'><button class='btn btn-danger px-1 py-0'>
+                          <svg
+                          width='0.9em'
+                          height='0.9em'
+                          viewBox='0 0 16 16'
+                          class='bi bi-trash'
+                          fill='currentColor'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'
+                          />
+                          <path
+                            fill-rule='evenodd'
+                            d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'
+                          />
+                          </svg>
+                        </button>
+                        </td>";
+                        }
+                      } else {
+                        echo "0 results";
+                      }
+                      $conn->close();
+                  ?>
                 </tr>
-               
               </tbody>
             </table>
           </div>
